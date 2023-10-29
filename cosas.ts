d12cab8a6a5e8aefb4b0329345e41634e67ded86
Nes <nes2504@hotmail.com>
2023-10-29 20:31:23 -0300

    primer commit

---
+++ cosas.ts
@@ -0,0 +1,15 @@
+interface Cosas {
+  id: number;
+  name: string;
+  descripcion: string;
+}
+
+const cosas: Cosas[] = [
+  { id: 1, name: "objeto1", descripcion: "descripcion del objeto 1" },
+  { id: 2, name: "objeto2", descripcion: "descripcion del objeto 2" },
+  { id: 3, name: "objeto3", descripcion: "descripcion del objeto 3" },
+];
+
+export function getAll() {}
+
+export function getById(Id: number) {}
---
+++ index.ts
@@ -0,0 +1,8 @@
+import { getAll, getById } from "./cosas";
+
+function main() {
+  //console.log(getAll());
+  console.log(getById(4));
+}
+
+main();
