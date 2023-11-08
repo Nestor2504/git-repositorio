89c3f3267ba7d212bdd9b0524be4cf37bc53fcc4
Nestor2504 <129322121+Nestor2504@users.noreply.github.com>
2023-11-07 21:42:15 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ function main() {
   //console.log(getAll());
   console.log(getById(4));
   console.log("este es un cambio en github");
+  console.log("este es otro cambio desde github");
 }
 
 main();
